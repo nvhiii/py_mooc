2adb2f45b0efdd33823e83df48cd9783cb884e9a
nvhiii <nahikhan16@gmail.com>
2024-04-01 10:44:13 -0400

    added exercise code for p1s3

---
+++ p1/section3/arithmetics.py
@@ -0,0 +1,11 @@
+# Write your solution here
+x = 27
+y = 15
+add = x + y
+sub = x - y
+mult = x * y
+div = x / y
+print(f"{x} + {y} = {add}")
+print(f"{x} - {y} = {sub}")
+print(f"{x} * {y} = {mult}")
+print(f"{x} / {y} = {div}")
