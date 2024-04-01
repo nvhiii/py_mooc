ef51178e9d2321b353df8146fff0414cfa89016f
nvhiii <nahikhan16@gmail.com>
2024-04-01 10:40:25 -0400

    added exercise code for p1s2

---
+++ p1/section2/name_addy.py
@@ -0,0 +1,8 @@
+# Write your solution here
+given_name = input("Given name: ")
+family_name = input("Family name: ")
+addy = input("Street address: ")
+postal = input("City and postal code: ")
+print(given_name + " " + family_name)
+print(addy)
+print(postal)
