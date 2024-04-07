7372ab6e0610079c16c7b747b5e3211afb96e1ed
nvhiii <nahikhan16@gmail.com>
2024-04-06 21:50:04 -0400

    working on p3 exercises

---
+++ p3/s3/mult.py
@@ -0,0 +1,12 @@
+# Write your solution here
+num = int(input("Please type in a number: "))
+first_operand = 1
+second_operand = 1
+
+while first_operand <= num:
+    print(f"{first_operand} x {second_operand} = {first_operand * second_operand}")
+    first_operand += 1
+    second_operand += 1
+
+    
+# WIP
