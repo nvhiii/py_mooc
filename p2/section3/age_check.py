28ecbe23d5b720718ee39935cd0707873f7c394e
nvhiii <nahikhan16@gmail.com>
2024-08-27 13:16:00 -0400

    p2s3 review ongoing

--- p2/section3/age_check.py
+++ p2/section3/age_check.py
@@ -1,9 +1,26 @@
-# Write your solution here
+# Please write a program which asks for the user's age. If the age 
+# is not plausible, that is, 
+# it is under 5 or something that can't be an actual human age, the
+#  program should print out a comment.
+
+# Have a look at the examples of expected behaviour below to figure out which comment is 
+# applicable in each case.
+
+# What is your age? 13
+# Ok, you're 13 years old
+
+# What is your age? 2
+# I suspect you can't write quite yet...
+
+# What is your age? -4
+# That must be a mistake
+
 age = int(input("What is your age? "))
 
-if age >= 5:
-    print(f"Ok, you're {age} years old")
-elif age >= 0 and age <= 4:
-    print(f"I suspect you can't write quite yet...")
+# first do break cases
+if age < 0:
+    print("That must be a mistake")
+elif age < 5:
+    print("I suspect you can't write quite yet...")
 else:
-    print(f"That must be a mistake")
+    print(f"Ok, you're {age} years old")
