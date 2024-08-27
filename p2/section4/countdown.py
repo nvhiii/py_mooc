e0bd329fe341584c7ac68838d4d345f3211b08d6
nvhiii <nahikhan16@gmail.com>
2024-08-27 14:50:02 -0400

    done w first test of loop

--- p2/section4/countdown.py
+++ p2/section4/countdown.py
@@ -1,9 +1,30 @@
+# This program should print out a countdown. The code is as follows:
+
+# number = 5
+# print("Countdown!")
+# while True:
+#   print(number)
+#   number = number - 1
+#   if number > 0:
+#     break
+
+# print("Now!")
+
 number = 5
+
 print("Countdown!")
 while True:
-  print(number)
-  number -= 1
-  if number < 1:
-    break
+    
+    # init
+    print(number)
+
+    # break stmnt
+    if number == 1:
+        break
+    
+    # if valid
+    number -= 1
 
 print("Now!")
+    
+    
