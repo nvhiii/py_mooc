2b0231e7c179dc1b57f86f341c4c2488a00273e2
nvhiii <nahikhan16@gmail.com>
2024-08-27 15:01:25 -0400

    pin attempts done

--- p2/section4/pin_attempts.py
+++ p2/section4/pin_attempts.py
@@ -1,16 +1,27 @@
-# Write your solution here
-attempt = 0
+# Please write a program which keeps asking the user for a PIN code 
+# until they type in the correct one, which is 4321. The program should 
+# then print out the number of times the user tried different codes.
+
+# PIN: 3245
+# Wrong
+# PIN: 1234
+# Wrong
+# PIN: 0000
+# Wrong
+# PIN: 4321
+# Correct! It took you 4 attempts
+
+attempts = 0
 
 while True:
-    pin = int(input("PIN: "))
-    attempt += 1
 
+    pin = int(input("PIN: "))
+    attempts += 1 # increment here bc each time input, that is an attempt
+
+    # break stmnt
     if pin == 4321:
         break
     else:
-        print(f"Wrong")
+        print("Wrong")
 
-if attempt == 1:
-    print(f"Correct! It only took you one single attempt!")
-else:
-    print(f"Correct! It took you {attempt} attempts")   
+print("Correct! It only took you one single attempt!") if attempts == 1 else print(f"Correct! It took you {attempts} attempts")
