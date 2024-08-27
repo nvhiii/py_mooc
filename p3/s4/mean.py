688d3b1bd79d34b1e851f6e2e5e7d263bbe266bd
nvhiii <nahikhan16@gmail.com>
2024-08-27 17:49:17 -0400

    p3s4 done, p3 done today

--- p3/s4/mean.py
+++ p3/s4/mean.py
@@ -1,3 +1,6 @@
+# Please write a function named mean, which takes three integer arguments. 
+# The function should print out the arithmetic mean of the three arguments
+
 # Write your code here
 def mean(x, y, z):
     sum = x + y + z
