89fc3f33b873d4557cb1763d0aea58c0bc23331e
nvhiii <nahikhan16@gmail.com>
2024-08-27 15:12:03 -0400

    countdown

--- p3/s1/countdown.py
+++ p3/s1/countdown.py
@@ -1,8 +1,28 @@
-# Fix the program
+# The program below has some syntactic issues:
+
+# print("Are you ready?")
+# number = int(input("Please type in a number: "))
+# while number = 0:
+# print(number)
+# print("Now!")
+
+# Please fix it so that it prints out the following:
+# Are you ready?
+# Please type in a number: 5
+# 5
+# 4
+# 3
+# 2
+# 1
+# Now!
+
 print("Are you ready?")
 number = int(input("Please type in a number: "))
-while number > 0:
+
+while not number == 0:
+
     print(number)
+
     number -= 1
 
 print("Now!")
