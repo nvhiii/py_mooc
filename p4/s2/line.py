0eef6b29a94c86664093bdb53d7778498ea21e54
nvhiii <nahikhan16@gmail.com>
2024-08-28 11:40:34 -0400

    lines done

--- p4/s2/line.py
+++ p4/s2/line.py
@@ -1,20 +1,23 @@
-# Write your solution here
+# Please write a function named line, which takes two arguments: 
+# an integer and a string. The function prints out a line of text, 
+# the length of which is specified by the first argument. The character 
+# used to draw the line should be the first character in the second 
+# argument. If the second argument is an empty string, the line should 
+# consist of stars.
 
-# Please write a function named line, which takes 
-# two arguments: an integer and a string. The function
-#  prints out a line of text, the length of which is 
-# specified by the first argument. The character used to
-# draw the line should be the first character in the 
-# second argument. If the second argument is an empty 
-# string, the line should consist of stars.
+# An example of expected behaviour:
 
-def line(num, str):
-    
-    if len(str) == 0:
-        print(f"*" * num) # accounts for new lines, and better time efficiency
-    else:
-        print(f"{str[0]}" * num)
+# line(7, "%")
+# line(10, "LOL")
+# line(3, "")
 
-# You can test your function by calling it within the following block
+# %%%%%%%
+# LLLLLLLLLL
+# ***
+
+def line(times, ustr):
+
+    print(times*ustr)
+
 if __name__ == "__main__":
-    line(5, "x")
+    line(7, "%")
