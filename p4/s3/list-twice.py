1b944fccde9d45c769ed0fe3e3e7b9deac71b29b
nvhiii <nahikhan16@gmail.com>
2024-08-28 15:50:01 -0400

    sorted list done

--- p4/s3/list-twice.py
+++ p4/s3/list-twice.py
@@ -1,23 +1,41 @@
-# Write your solution here
-# Please write a program which asks the user to 
-# type in values and adds them to a list. After 
-# each addition, the list is printed out in two different ways:
+# Please write a program which asks the user to type in values 
+# and adds them to a list. After each addition, the list is printed out 
+# in two different ways:
 
 # in the order the items were added
 # ordered from smallest to greatest
 # The program exits when the user types in 0.
 
+# An example of expected behaviour:
+
+# New item: 3
+# The list now: [3]
+# The list in order: [3]
+# New item: 1
+# The list now: [3, 1]
+# The list in order: [1, 3]
+# New item: 9
+# The list now: [3, 1, 9]
+# The list in order: [1, 3, 9]
+# New item: 5
+# The list now: [3, 1, 9, 5]
+# The list in order: [1, 3, 5, 9]
+# New item: 0
+# Bye!
+
 my_list = []
 
 while True:
 
-    # prompt input for list
-    value = int(input("New item: "))
-    if value == 0:
+    num = int(input("New item: "))
+    if num == 0:
         print("Bye!")
         break
-    
-    my_list.append(value)
-    sortList = sorted(my_list)
+
+    my_list.append(num)
+    sort_list = sorted(my_list)
     print(f"The list now: {my_list}")
-    print(f"The list in order: {sortList}")
+    print(f"The list in order: {sort_list}")
+        
+
+
