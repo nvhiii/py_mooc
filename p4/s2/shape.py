b190ce14f9dc0f6ff24313cf36b03e9d967f0b68
nvhiii <nahikhan16@gmail.com>
2024-08-28 13:14:38 -0400

    shapes done

--- p4/s2/shape.py
+++ p4/s2/shape.py
@@ -1,36 +1,59 @@
-# Please write a function named shape, which takes 
-# four arguments. The first two parameters specify 
-# a triangle, as above, and the character used to 
-# draw it. The first parameter also specifies the 
-# width of a rectangle, while the third parameter 
-# specifies its height. The fourth parameter specifies 
-# the filler character of the rectangle. The function 
-# prints first the triangle, and then the rectangle below it.
+# Please write a function named shape, which takes four arguments. 
+# The first two parameters specify a triangle, as above, and the 
+# character used to draw it. The first parameter also specifies the 
+# width of a rectangle, while the third parameter specifies its height. 
+# The fourth parameter specifies the filler character of the rectangle.
+# The function prints first the triangle, and then the rectangle below it.
 
-# The function should call the function line from the 
-# exercise above for the actual printing out. Copy 
-# your solution to that exercise above the code for 
-# this exercise. Please don't change anything in the line function.
+# The function should call the function line from the exercise above 
+# for the actual printing out. Copy your solution to that exercise 
+# above the code for this exercise. Please don't change anything in 
+# the line function.
 
-# Copy here code of line function from previous exercise and use it in your solution
+# Some examples:
 
-def line(num, str):
-    
-    if len(str) == 0:
-        print(f"*" * num) # accounts for new lines, and better time efficiency
+# shape(5, "X", 3, "*")
+# print()
+# shape(2, "o", 4, "+")
+# print()
+# shape(3, ".", 0, ",")
+
+# X
+# XX
+# XXX
+# XXXX
+# XXXXX
+# *****
+# *****
+# *****
+
+# o
+# oo
+# ++
+# ++
+# ++
+# ++
+
+# .
+# ..
+# ...
+
+def line(times, ustr):
+
+    if ustr=="":
+        print(f"*" * times)
     else:
-        print(f"{str[0]}" * num)
+        print(times*ustr[0])
 
-def shape(triangle_height, tri_char, rect_height, rect_char):
-    i = 1
-    while i <= triangle_height:
-        line(i, tri_char)
-        i += 1
-    j = 1
-    while j <= rect_height:
-        line(triangle_height, rect_char)
-        j += 1
+def shape(dim, ustr1, height, ustr2):
 
-# You can test your function by calling it within the following block
-if __name__ == "__main__":
-    shape(5, "x", 2, "o")
+    # print out triangle then square
+    num = 1
+    while num <= dim:
+        line(num, ustr1)
+        num += 1
+
+    num_rect = 0
+    while num_rect < height:
+        line(dim, ustr2)
+        num_rect += 1
