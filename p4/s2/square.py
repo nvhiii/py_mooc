103a6645127269765381856e9ef76f61e9ebd996
nvhiii <nahikhan16@gmail.com>
2024-08-28 12:59:06 -0400

    square py done

--- p4/s2/square.py
+++ p4/s2/square.py
@@ -1,29 +1,39 @@
-# Please write a function named square, which prints out
-#  a square of characters, and takes two arguments. The first 
-#parameter specifies the length of the side of the square. 
-# The second parameter specifies the character used to draw the square.
+# Please write a function named square, which prints out a square of 
+# characters, and takes two arguments. The first parameter specifies t
+# he length of the side of the square. The second parameter specifies 
+# he character used to draw the square.
 
-# The function should call the function line from the exercise
-# above for the actual printing out. Copy your solution to that 
-#exercise above the code for this exercise. Please don't change 
-# anything in the line function.
+# The function should call the function line from the exercise above 
+# for the actual printing out. Copy your solution to that exercise above 
+# the code for this exercise. Please don't change anything in the line 
+# function.
 
-# Copy here code of line function from previous exercise
+# Some examples:
 
-def line(num, str):
-    
-    if len(str) == 0:
-        print(f"*" * num) # accounts for new lines, and better time efficiency
+# square(5, "*")
+# print()
+# square(3, "o")
+
+# *****
+# *****
+# *****
+# *****
+# *****
+
+# ooo
+# ooo
+# ooo
+
+def line(times, ustr):
+
+    if ustr=="":
+        print(f"*" * times)
     else:
-        print(f"{str[0]}" * num)
+        print(times*ustr[0])
 
-def square(size, character):
-    # You should call function line here with proper parameters
-    i = 1
-    while i <= size:
-        line(size, character) # generic size and chara
-        i += 1
+def square(amt, ustr):
 
-# You can test your function by calling it within the following block
-if __name__ == "__main__":
-    square(5, "x")
+    num = 0
+    while num < amt:
+        line(amt, ustr) # width is amt, since this is a square
+        num += 1
