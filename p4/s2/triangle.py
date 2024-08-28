311bdca72fc4a6ef808ab7c3e0632e5cf654fa91
nvhiii <nahikhan16@gmail.com>
2024-08-28 13:07:32 -0400

    trinagle py done

--- p4/s2/triangle.py
+++ p4/s2/triangle.py
@@ -1,27 +1,39 @@
-# Please write a function named triangle, which draws a
-# triangle of hashes, and takes one argument. The triangle 
-#should be as tall and as wide as the value of the argument.
+# Please write a function named triangle, which draws a triangle 
+# of hashes, and takes one argument. The triangle should be as tall 
+# and as wide as the value of the argument.
 
-# The function should call the function line from the 
-# exercise above for the actual printing out. Copy your 
-# solution to that exercise above the code for this
-# exercise. Please don't change anything in the line function.
+# The function should call the function line from the exercise above 
+# for the actual printing out. Copy your solution to that exercise 
+# above the code for this exercise. Please don't change anything in 
+# the line function.
 
-# Copy here code of line function from previous exercise
-def line(num, str):
-    
-    if len(str) == 0:
-        print(f"*" * num) # accounts for new lines, and better time efficiency
+# Some examples:
+
+# triangle(6)
+# print()
+# triangle(3)
+
+# #
+# ##
+# ###
+# ####
+# #####
+# ######
+
+# #
+# ##
+# ###
+
+def line(times, ustr):
+
+    if ustr=="":
+        print(f"*" * times)
     else:
-        print(f"{str[0]}" * num)
+        print(times*ustr[0])
 
-def triangle(size):
-    # You should call function line here with proper parameters
-    i = 1
-    while i <= size:
-        line(i, "#")
-        i += 1
+def triangle(dim):
 
-# You can test your function by calling it within the following block
-if __name__ == "__main__":
-    triangle(5)
+    num = 1
+    while num <= dim:
+        line(num, "#")
+        num += 1
