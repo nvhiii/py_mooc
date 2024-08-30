4fa45adb34f6268f964fb36bfd9c2df088d08a5c
nvhiii <nahikhan16@gmail.com>
2024-08-30 17:12:57 -0400

    formatted p4s5 done

--- p4/s5/integers_to_string.py
+++ p4/s5/integers_to_string.py
@@ -1,18 +1,23 @@
-# Write your solution here
-# Please write a function named formatted, which takes a 
-# list of floating point numbers as its argument. The function 
-# returns a new list, which contains each element of the original 
-# list in string format, rounded to two decimal points. The order 
-# of the items in the list should remain unchanged.
+# Please write a function named formatted, which takes a list of 
+# floating point numbers as its argument. The function returns a 
+# new list, which contains each element of the original list in 
+# string format, rounded to two decimal points. The order of the 
+# items in the list should remain unchanged.
 
-#Hint: use f-strings to format the floating point numbers into suitable strings.
+# Hint: use f-strings to format the floating point numbers into suitable strings.
 
-def formatted(list):
+# An example of expected beahviour:
 
-    formattedList = []
+# my_list = [1.234, 0.3333, 0.11111, 3.446]
+# new_list = formatted(my_list)
+# print(new_list)
 
-    for decimal in list:
-        format = f"{decimal:.2f}"
-        formattedList.append(format)
+# ['1.23', '0.33', '0.11', '3.45']
 
-    return formattedList
+def formatted(my_list: list) -> list:
+    new_list = []
+    for i in my_list:
+        formatter = f"{i:.2f}"
+        new_list.append(formatter)
+
+    return new_list
