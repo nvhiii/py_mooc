ec395b32ba881abfaea145e1152700787ec67624
nvhiii <nahikhan16@gmail.com>
2024-08-30 17:03:45 -0400

    longest done

--- p4/s4/all_longest_in_list.py
+++ p4/s4/all_longest_in_list.py
@@ -1,40 +1,24 @@
-# Write your solution here
-# Please write a function named all_the_longest, which 
-# takes a list of strings as its argument. The function
-# should return a new list containing the longest string in 
-# the original list. If more than one are equally long, the 
-# function should return all of the longest strings.
+# Please write a function named all_the_longest, which takes a list of strings 
+# as its argument. The function should return a new list containing the longest 
+# string in the original list. If more than one are equally long, the function 
+# should return all of the longest strings.
 
-# The order of the strings in the returned list should 
-# be the same as in the original.
+# The order of the strings in the returned list should be the same as in 
+# the original.
 
-def all_the_longest(list):
+# my_list = ["first", "second", "fourth", "eleventh"]
 
-    maxString = ""
-    newList = []
-    iterator = 0
+# result = all_the_longest(my_list)
+# print(result) # ['eleventh']
 
-    while True:
+# my_list = ["adele", "mark", "dorothy", "tim", "hedy", "richard"]
 
-        if iterator == len(list):
-            break
+# result = all_the_longest(my_list)
+# print(result) # ['dorothy', 'richard']
 
-            # set max string
-        if (iterator == 0):
-            maxString = list[0]
-            newList.append(maxString)
-            iterator += 1
-            # update max
-        elif (len(list[iterator]) > len(maxString)):
-            newList.clear()
-            maxString = list[iterator]
-            newList.append(maxString)
-            iterator += 1
-        elif (len(list[iterator]) == len(maxString)):
-            # if string lengths equal
-            newList.append(list[iterator])
-            iterator += 1
-        else:
-            iterator += 1
+def all_the_longest(my_list: list) -> list:
 
-    return newList
+    longest = max(len(s) for s in my_list)
+
+    longest_strings = [s for s in my_list if len(s) == longest]
+    return longest_strings
