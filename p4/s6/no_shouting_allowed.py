0eb110639ee67a4f93ce26acf8454dcfc6ecbe6c
nvhiii <nahikhan16@gmail.com>
2024-08-30 17:51:04 -0400

    no shouting allowed done

--- p4/s6/no_shouting_allowed.py
+++ p4/s6/no_shouting_allowed.py
@@ -1,21 +1,32 @@
-# The Python string method isupper() returns True if a 
-# string consists of only uppercase characters.
-#
-# Please use the isupper method to write a function named no_shouting, which takes 
-# a list of strings as an argument. The function returns a new 
-# list, containing only those items from the original which do not 
+# The Python string method isupper() returns True if a string consists 
+# of only uppercase characters.
+
+# Some examples:
+
+# print("XYZ".isupper())
+
+# is_it_upper = "Abc".isupper()
+# print(is_it_upper)
+
+# True
+# False
+
+# Please use the isupper method to write a function named no_shouting,
+#  which takes a list of strings as an argument. The function returns 
+# a new list, containing only those items from the original which do not 
 # consist of solely uppercase characters.
 
-def no_shouting(list):
+# An example of expected behaviour:
 
-    iterator = 0
-    lowercase = []
+# my_list = ["ABC", "def", "UPPER", "ANOTHERUPPER", "lower", "another lower", 
+# "Capitalized"]
+# pruned_list = no_shouting(my_list)
+# print(pruned_list)
 
-    while iterator < len(list):
+# ['def', 'lower', 'another lower', 'Capitalized']
 
-        if not (list[iterator].isupper()):
-            lowercase.append(list[iterator])
+def no_shouting(my_list: list) -> list:
 
-        iterator += 1
-
+    lowercase = [s for s in my_list if not s.isupper()]
     return lowercase
+    
