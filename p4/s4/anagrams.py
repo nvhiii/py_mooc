9e4e1b5031c76acc1554af6d4e6ca450a8554c3a
nvhiii <nahikhan16@gmail.com>
2024-08-30 16:30:03 -0400

    anagrams done

--- p4/s4/anagrams.py
+++ p4/s4/anagrams.py
@@ -1,12 +1,17 @@
-# Write your solution here
-# Please write a function named anagrams, which takes 
-# two strings as arguments. The function returns True 
-# if the strings are anagrams of each other. Two words 
-# are anagrams if they contain exactly the same characters.
+# Please write a function named anagrams, which takes two strings as 
+# arguments. The function returns True if the strings are anagrams 
+# of each other. Two words are anagrams if they contain exactly the 
+# same characters.
 
-def anagrams(str1, str2):
+# Some examples of how the function should work:
 
-    if sorted(str1) == sorted(str2):
-        return True
-    else:
-        return False
+# print(anagrams("tame", "meta")) # True
+# print(anagrams("tame", "mate")) # True
+# print(anagrams("tame", "team")) # True
+# print(anagrams("tabby", "batty")) # False
+# print(anagrams("python", "java")) # False
+
+# Hint: the function sorted can be used on strings as well.
+
+def anagrams(val: str, val2: str) -> bool:
+    return sorted(val) == sorted(val2)
