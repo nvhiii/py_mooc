3ee8eee3f1126e77d5e9bf6ec624d4ef2ec6cdd6
nvhiii <nahikhan16@gmail.com>
2024-08-30 17:36:02 -0400

    most common chara done using dictionary

--- p4/s6/most_comon_character.py
+++ p4/s6/most_comon_character.py
@@ -1,30 +1,22 @@
-# Write your solution here
-# Please write a function named most_common_character, which 
-# takes a string argument. The function returns the character 
-# which has the most occurrences within the string. If there are 
-# many characters with equally many occurrences, the one which 
-# appears first in the string should be returned.
-
-def most_common_character(str):
-
-    # var to hold the mode
-    mostOccurence = ""
-    mostValue = 0
-    iterator = 0
-
-    # find most occurences
-    while iterator < len(str):
-        
-        if (iterator == 0):
-            mostOccurence = str[0]
-            mostValue = str.count(mostOccurence)
-            iterator += 1
-        elif (str.count(str[iterator]) > mostValue):
-            mostOccurence = str[iterator]
-            mostValue = str.count(mostOccurence)
-            iterator += 1
+def most_common_character(s: str) -> str:
+    # Step 1: Initialize a dictionary to count character frequencies
+    frequency = {}
+    
+    # Step 2: Count the frequency of each character in the string
+    for char in s:
+        if char in frequency:
+            frequency[char] += 1
         else:
-            iterator += 1
-
-
-    return mostOccurence
+            frequency[char] = 1
+    
+    # Step 3: Find the most common character
+    max_count = 0
+    most_common_char = ''
+    
+    for char in s:
+        if frequency[char] > max_count:
+            max_count = frequency[char]
+            most_common_char = char
+    
+    # Step 4: Return the most common character
+    return most_common_char
