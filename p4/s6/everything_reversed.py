84ded6e135a27344b001e2682b316e3d1f6b0008
nvhiii <nahikhan16@gmail.com>
2024-08-30 17:19:52 -0400

    completely reversed lists

--- p4/s6/everything_reversed.py
+++ p4/s6/everything_reversed.py
@@ -1,23 +1,18 @@
-# Write your solution here
-# Please write a function named everything_reversed, which takes 
-# a list of strings as its argument. The function returns a new 
-# list with all of the items on the original list reversed. 
-# Also the order of items should be reversed on the new list.
+# Please write a function named everything_reversed, which takes a 
+# list of strings as its argument. The function returns a new list 
+# with all of the items on the original list reversed. Also the order
+# of items should be reversed on the new list.
 
-def everything_reversed(list):
+# An example of how the function should work:
 
-    reverseEverything = []
-    lastIndex = len(list) - 1
+# my_list = ["Hi", "there", "example", "one more"]
+# new_list = everything_reversed(my_list)
+# print(new_list)
 
-    while lastIndex >= 0:
+# ['erom eno', 'elpmaxe', 'ereht', 'iH']
 
-        # order of items swapped
-        # now reverse each string
-        reversedString = list[lastIndex][::-1]
-        reverseEverything.append(reversedString)
+def everything_reversed(my_list: list) -> list:
 
-        lastIndex -= 1
-
-    return reverseEverything
-
+    reverser = [s[::-1] for s in my_list[::-1]] # reverse strings, and start at last index too
+    return reverser
 
