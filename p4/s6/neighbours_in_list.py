2096bd0039c78082c31ffaa048577516ee34b8b3
nvhiii <nahikhan16@gmail.com>
2024-09-02 14:22:14 -0400

    redid no shouting and neighbors in list

--- p4/s6/neighbours_in_list.py
+++ p4/s6/neighbours_in_list.py
@@ -1,30 +1,34 @@
-# Given a list of integers, let's decide that two consecutive items 
-# in the list are neighbours if their difference is 1. So, items 1 
-# and 2 would be neighbours, and so would items 56 and 55.
+# # Given a list of integers, let's decide that 
+# two consecutive items in the list are neighbours 
+# if their difference is 1. So, items 1 and 2 would 
+# be neighbours, and so would items 56 and 55.
 
-# Please write a function named longest_series_of_neighbours, which looks 
-# for the longest series of neighbours within the list, and returns its length.
+# # Please write a function named longest_series_of_neighbours, 
+# which looks for the longest series of neighbours within the list, 
+# and returns its length.
 
-# For example, in the list [1, 2, 5, 4, 3, 4] the longest list of neighbours 
-# would be [5, 4, 3, 4], with a length of 4.
+# # For example, in the list [1, 2, 5, 4, 3, 4] the longest list of
+#  neighbours would be [5, 4, 3, 4], with a length of 4.
 
-# An example function call:
+# # An example function call:
 
-# my_list = [1, 2, 5, 7, 6, 5, 6, 3, 4, 1, 0]
-# print(longest_series_of_neighbours(my_list))
+# # my_list = [1, 2, 5, 7, 6, 5, 6, 3, 4, 1, 0]
+# # print(longest_series_of_neighbours(my_list))
 
-# 4
+# # 4
 
 def longest_series_of_neighbours(my_list: list) -> int:
 
-    max_length = 1
-    current_length = 1
+    max_length = 1 # why are these one
+    current_length = 1 # why are these one
+
     for i in range(1, len(my_list)):
-        if abs(my_list[i] - my_list[i-1]) == 1:
+        if abs(my_list[i] - my_list[i - 1]) == 1:
             current_length += 1
         else:
-            max_length = max(max_length, current_length)
-            current_length = 1
+            max_length = max(max_length, current_length) # using max fxn to check which is the max
+            current_length = 1 # reset when current length diff isnt abs of 1
 
-    max_length = max(max_length, current_length)
+    max_length = max(max_length, current_length) # need to review why this is the case
     return max_length
+
--- p4/s6/no_shouting_allowed.py
+++ p4/s6/no_shouting_allowed.py
@@ -1,32 +1,38 @@
-# The Python string method isupper() returns True if a string consists 
-# of only uppercase characters.
+# # The Python string method isupper() returns True if a string 
+# consists of only uppercase characters.
 
-# Some examples:
+# # Some examples:
 
-# print("XYZ".isupper())
+# # print("XYZ".isupper())
 
-# is_it_upper = "Abc".isupper()
-# print(is_it_upper)
+# # is_it_upper = "Abc".isupper()
+# # print(is_it_upper)
 
-# True
-# False
+# # True
+# # False
 
-# Please use the isupper method to write a function named no_shouting,
-#  which takes a list of strings as an argument. The function returns 
-# a new list, containing only those items from the original which do not 
-# consist of solely uppercase characters.
+# # Please use the isupper method to write a function named 
+# no_shouting, which takes a list of strings as an argument. 
+# The function returns a new list, containing only those items 
+# from the original which do not consist of solely uppercase characters.
 
-# An example of expected behaviour:
+# # An example of expected behaviour:
 
-# my_list = ["ABC", "def", "UPPER", "ANOTHERUPPER", "lower", "another lower", 
-# "Capitalized"]
-# pruned_list = no_shouting(my_list)
-# print(pruned_list)
+# # my_list = ["ABC", "def", "UPPER", "ANOTHERUPPER", "lower", 
+# "another lower", "Capitalized"]
+# # pruned_list = no_shouting(my_list)
+# # print(pruned_list)
 
-# ['def', 'lower', 'another lower', 'Capitalized']
+# # ['def', 'lower', 'another lower', 'Capitalized']
 
 def no_shouting(my_list: list) -> list:
 
-    lowercase = [s for s in my_list if not s.isupper()]
-    return lowercase
-    
+    lower = [s for s in my_list if not s.isupper()]
+    return lower
+
+def main():
+    my_list = ["ABC", "def", "UPPER", "ANOTHERUPPER", "lower", "another lower", "Capitalized"]
+    pruned_list = no_shouting(my_list)
+    print(pruned_list)
+
+main()
