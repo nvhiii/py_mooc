9b9423b1065e368dec75c29b1372f2046b2aa06e
nvhiii <nahikhan16@gmail.com>
2024-09-06 13:10:33 -0400

    done

--- p5/s1/longest_string.py
+++ p5/s1/longest_string.py
@@ -12,8 +12,9 @@
 # howdydoody
 
 def longest(strings: list) -> str:
-    current_longest = strings[0]
+    longest = strings[0]
     for string in strings:
-        if len(string) > len(current_longest):
-            current_longest = string
-    return current_longest
+        if len(string) > len(longest):
+            longest = string
+
+    return longest
