6be286b2c22725c8aaebb0e77d30ce7e8a5c604c
nvhiii <nahikhan16@gmail.com>
2024-09-15 18:20:00 -0400

    times ten

---
+++ p5/s3/times_ten.py
@@ -0,0 +1,18 @@
+# Please write a function named times_ten(start_index: int, end_index: int), which creates and returns a new dictionary. The keys of the dictionary should be the numbers between start_index and end_index inclusive
+
+# The value mapped to each key should be the key times ten.
+
+# For example:
+
+# d = times_ten(3, 6)
+# print(d)
+
+# {3: 30, 4: 40, 5: 50, 6: 60}
+
+def times_ten(start_index: int, end_index: int):
+
+    my_dict = {}
+    for i in range(start_index, end_index + 1):
+        my_dict[i] = i * 10
+
+    return my_dict
