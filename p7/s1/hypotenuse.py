875de1e32429a414e0313ecf7702a28b835a71a9
nvhiii <nahikhan16@gmail.com>
2024-10-02 19:47:13 -0400

    hypo

--- p7/s1/hypotenuse.py
+++ p7/s1/hypotenuse.py
@@ -7,3 +7,10 @@
 # print(hypotenuse(3,4)) # 5.0
 # print(hypotenuse(5,12)) # 13.0
 # print(hypotenuse(1,1)) # 1.4142135623730951
+
+from math import sqrt
+
+def hypotenuse(leg1: float, leg2: float):
+    return sqrt(leg1**2 + leg2**2)
+
+
