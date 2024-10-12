17a6e73a67f1f91a4c22991478551c6eb4f57af3
nvhiii <nahikhan16@gmail.com>
2024-10-12 12:34:56 -0400

    pets

---
+++ p9/s2/pets.py
@@ -0,0 +1,28 @@
+# WRITE YOUR SOLUTION HERE:
+
+class Pet:
+    def __init__(self, name: str, description: str):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        return f"{self.name} ({self.description})"
+
+class Person:
+    def __init__(self, name: str, pet: Pet):
+        self.name = name
+        self.pet = pet
+
+    def __str__(self):
+        return f"{self.name}, whose pal is {self.pet.name}, a {self.pet.description}"
+    
+# The exercise template contains the outlines of two classes: Person and Pet. Each person has one pet. Please change the __str__ method in the class Person so that it also prints out information about the person's pet as shown in the example below.
+
+# The string returned by the method must follow the format specified below exactly.
+
+# hulda = Pet("Hulda", "mixed-breed dog")
+# levi = Person("Levi", hulda)
+
+# print(levi)
+
+# Levi, whose pal is Hulda, a mixed-breed dog
