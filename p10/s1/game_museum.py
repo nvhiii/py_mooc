a356e3090b61b08aeba4464049b365cb505679b2
nvhiii <nahikhan16@gmail.com>
2024-10-15 13:33:06 -0400

    gm

---
+++ p10/s1/game_museum.py
@@ -0,0 +1,49 @@
+# TEE RATKAISUSI TÄHÄN:
+class ComputerGame:
+    def __init__(self, name: str, publisher: str, year: int):
+        self.name = name
+        self.publisher = publisher
+        self.year = year
+
+class GameWarehouse:
+    def __init__(self):
+        self.__games = []
+
+    def add_game(self, game: ComputerGame):
+        self.__games.append(game)
+
+    def list_games(self):
+        return self.__games
+
+# The exercise template contains class definitions for a ComputerGame and a GameWarehouse. A GameWarehouse object is used to store ComputerGame objects.
+
+# Please familiarize yourself with these classes. Then define a new class named GameMuseum which inherits the GameWarehouse class.
+
+# The GameMuseum class should override the list_games() method, so that it returns a list of only those games which were made before the year 1990.
+
+# The new class should also have a constructor which calls the constructor from the parent class GameWarehouse. The constructor takes no arguments.
+
+# You may use the following code to test your implementation:
+
+# museum = GameMuseum()
+# museum.add_game(ComputerGame("Pacman", "Namco", 1980))
+# museum.add_game(ComputerGame("GTA 2", "Rockstar", 1999))
+# museum.add_game(ComputerGame("Bubble Bobble", "Taito", 1986))
+# for game in museum.list_games():
+#     print(game.name)
+
+# Pacman
+# Bubble Bobble
+
+class GameMuseum(GameWarehouse):
+    def __init__(self):
+        super().__init__() # since no nother class atr, can just call super init
+
+    def list_games(self):
+        og_list = super().list_games()
+        before_ninety = []
+        for game in og_list:
+            if game.year < 1990:
+                before_ninety.append(game)
+
+        return before_ninety
