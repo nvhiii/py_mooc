f86b604daf247491aa8281c496c38f48966769e9
nvhiii <nahikhan16@gmail.com>
2024-10-17 20:14:24 -0400

    p12cr

--- p10/s4/course_records.py
+++ p10/s4/course_records.py
@@ -88,6 +88,14 @@ class Course():
     def grade(self, val: int):
         self.__grade = val
 
+    @property
+    def credits(self):
+        return self.__credits
+    
+    @credits.setter
+    def credits(self, val: int):
+        self.__credits = val
+
     def __str__(self):
         return f"{self.__cname} ({self.__credits} cr) grade {self.__grade}"
 
@@ -107,6 +115,35 @@ class CourseList():
         if name not in self.__courses:
             return None
         return self.__courses[name] # return entire course obj
+
+    def get_courses_len(self):
+        return len(self.__courses)
+    
+    def total_credits(self):
+        total = 0
+        for course in self.__courses:
+            total += self.__courses[course].credits # accesses the credits getter
+        return total
+    
+    def avg_grade(self):
+        dividend = 0 # this will keep track of total of grades
+        divisor = self.get_courses_len()
+        for course in self.__courses:
+            dividend += self.__courses[course].grade # access grade getter
+        mean_grade = dividend / divisor
+        return mean_grade
+    
+    def grades(self):
+        grds = {}
+        for course in self.__courses:
+            grd = self.__courses[course].grade # give course obj grade
+            if grd not in grds:
+                grds[grd] = 1
+            else:
+                grds[grd] += 1
+
+        return grds
+        # this method returns a dict of {grade: num of times appeared, etc...}
 
 class CourseRecordsApplication():
     def __init__(self):
@@ -130,7 +167,19 @@ class CourseRecordsApplication():
         entire_course = self.__cl.get_c(course)
         if entire_course == None:
             print("no entry for this course")
+            return # need return or else returns none
         print(entire_course)
+
+    def stats(self):
+        length = self.__cl.get_courses_len()
+        creds = self.__cl.total_credits()
+        avg_grade = self.__cl.avg_grade()
+        grades = self.__cl.grades()
+        print(f"{length} completed courses, a total of {creds} credits")
+        print(f"mean {avg_grade:.1f}")
+        print("grade distribution")
+        for i in range(1, 6):
+            print(f"{i}: {'x' * grades.get(i, 0)}")
 
     def execute(self):
         self.prompts()
@@ -143,6 +192,8 @@ class CourseRecordsApplication():
                 self.add_course()
             if cmd == "2":
                 self.get_course_data()
+            if cmd == "3":
+                self.stats()
             
 app = CourseRecordsApplication()
 app.execute()
