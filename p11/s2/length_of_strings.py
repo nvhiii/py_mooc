7ca6cb7bf3189b5a1ad2309dd1ad335de9ae95f5
nvhiii <nahikhan16@gmail.com>
2024-10-18 16:36:04 -0400

    los

---
+++ p11/s2/length_of_strings.py
@@ -0,0 +1,15 @@
+# Please write a function named lengths(strings: list) which takes a list of strings as its argument. The function should return a dictionary with the strings in the list as the keys and their lengths as the values.
+
+# The function should be implemented with a dictionary comprehension. The maximum length of the function is two lines of code, including the header line beginning with the def keyword.
+
+# The function should work as follows:
+
+# word_list = ["once", "upon" , "a", "time", "in"]
+
+# word_lengths = lengths(word_list)
+# print(word_lengths)
+
+# {'once': 4, 'upon': 4, 'a': 1, 'time': 4, 'in': 2}
+
+def lengths(strings: list):
+    return {string : len(string) for string in strings}
