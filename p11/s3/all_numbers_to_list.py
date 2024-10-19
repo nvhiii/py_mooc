2649c97eb8af51dd9aa98e6f36c5c0e5bd44b44d
nvhiii <nahikhan16@gmail.com>
2024-10-19 14:29:04 -0400

    antl

---
+++ p11/s3/all_numbers_to_list.py
@@ -0,0 +1,14 @@
+# Please write a recursive function named add_numbers_to_list(numbers: list). The function takes a list of numbers as its argument, and adds new numbers to the list until the length of the list is divisible by five. Each number added to the list should be one greater than the last number in the list.
+
+# The function must call itself recursively. Please see the example below.
+
+# numbers = [1,3,4,5,10,11]
+# add_numbers_to_list(numbers)
+# print(numbers)
+
+# [1, 3, 4, 5, 10, 11, 12, 13, 14, 15]
+
+def add_numbers_to_list(numbers: list):
+    if not len(numbers) % 5 == 0:
+        numbers.append(numbers[-1] + 1)
+        add_numbers_to_list(numbers)
