5a05daf366db025804b2de23366c053145e2df92
nvhiii <nahikhan16@gmail.com>
2024-10-22 21:44:49 -0400

    rp

---
+++ p12/s1/ratings.py
@@ -0,0 +1,19 @@
+# Please write a function named sort_by_ratings(items: list) which takes a list of dictionaries as its argument. The structure of the dictionaries is identical to the previous exercise. This function should sort the dictionaries in descending order based on the shows' ratings. The function should not change the original list, but return a new list instead.
+
+# shows = [{ "name": "Dexter", "rating" : 8.6, "seasons":9 }, { "name": "Friends", "rating" : 8.9, "seasons":10 },  { "name": "Simpsons", "rating" : 8.7, "seasons":32 }  ]
+
+# print("Rating according to IMDB")
+# for show in sort_by_ratings(shows):
+#     print(f"{show['name']}  {show['rating']}")
+
+# Rating according to IMDB
+# Friends 8.9
+# Simpsons 8.7
+# Dexter 8.6
+
+def sort_by_ratings(items: list):
+
+    def rating(show: tuple):
+        return show["rating"]
+    
+    return sorted(items, key=rating, reverse=True)
